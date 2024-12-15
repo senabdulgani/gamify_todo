e2a4c947718629cc1eca4b5de6fa979dd90a461f
Kargalar <m.islam0422@gmail.com>
2024-12-15 22:16:08 +0300

    fix routine actions oane

--- lib/3 Page/Home/Widget/task_slide_actions.dart
+++ lib/3 Page/Home/Widget/task_slide_actions.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:gamify_todo/1%20Core/helper.dart';
 import 'package:gamify_todo/2%20General/app_colors.dart';
 import 'package:gamify_todo/5%20Service/locale_keys.g.dart';
 import 'package:gamify_todo/6%20Provider/task_provider.dart';
@@ -64,38 +65,41 @@ class _TaskSlideActinosState extends State<TaskSlideActinos> {
                 ),
               ],
             ),
-      startActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        extentRatio: 0.5,
-        closeThreshold: 0.1,
-        openThreshold: 0.1,
-        dismissible: DismissiblePane(
-          dismissThreshold: 0.01,
-          onDismissed: () {
-            taskProvider.failedTask(widget.taskModel);
-          },
-        ),
-        children: [
-          SlidableAction(
-            onPressed: (context) {
-              taskProvider.failedTask(widget.taskModel);
-            },
-            backgroundColor: AppColors.red,
-            icon: Icons.close,
-            label: LocaleKeys.Failed.tr(),
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-          ),
-          SlidableAction(
-            onPressed: (context) {
-              taskProvider.cancelTask(widget.taskModel);
-            },
-            backgroundColor: AppColors.purple,
-            icon: Icons.block,
-            label: LocaleKeys.Cancel.tr(),
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-          ),
-        ],
-      ),
+
+      startActionPane: widget.taskModel.routineID != null && !Helper().isSameDay(widget.taskModel.taskDate, DateTime.now())
+          ? null
+          : ActionPane(
+              motion: const ScrollMotion(),
+              extentRatio: 0.5,
+              closeThreshold: 0.1,
+              openThreshold: 0.1,
+              dismissible: DismissiblePane(
+                dismissThreshold: 0.01,
+                onDismissed: () {
+                  taskProvider.failedTask(widget.taskModel);
+                },
+              ),
+              children: [
+                SlidableAction(
+                  onPressed: (context) {
+                    taskProvider.failedTask(widget.taskModel);
+                  },
+                  backgroundColor: AppColors.red,
+                  icon: Icons.close,
+                  label: LocaleKeys.Failed.tr(),
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                ),
+                SlidableAction(
+                  onPressed: (context) {
+                    taskProvider.cancelTask(widget.taskModel);
+                  },
+                  backgroundColor: AppColors.purple,
+                  icon: Icons.block,
+                  label: LocaleKeys.Cancel.tr(),
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                ),
+              ],
+            ),
       child: widget.child,
     );
   }
