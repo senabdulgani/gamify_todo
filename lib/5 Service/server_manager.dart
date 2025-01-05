60f2a74043c37a524b4e33e86ff195d3f47dba6e
Kargalar <m.islam0422@gmail.com>
2025-01-05 21:01:31 +0300

    heoku

--- lib/5 Service/server_manager.dart
+++ lib/5 Service/server_manager.dart
@@ -20,8 +20,8 @@ class ServerManager {
   }
 
   // static const String _baseUrl = 'http://localhost:3001';
-  static const String _baseUrl = 'http://192.168.1.21:3001';
-  // static const String _baseUrl = 'https://gamify-273bac1e9487.herokuapp.com';
+  // static const String _baseUrl = 'http://192.168.1.21:3001';
+  static const String _baseUrl = 'https://gamify-273bac1e9487.herokuapp.com';
 
   var dio = Dio();
   // --------------------------------------------w
